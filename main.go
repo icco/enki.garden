e7758e098d1aa2fe1c0b569dff19613fa2859a5c
Nat Welch <nat@natwelch.com>
2016-03-31 03:41:45 +0000

    add basic server

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	const tpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>enki.garden</title>
+  </head>
+  <body>
+    <p>Welcome!</p>
+  </body>
+</html>
+`
+
+	tmpl, err := template.New("name").Parse(tpl)
+	if err != nil {
+		log.Printf("Template Parse: %+v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Template Exec: %+v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
+func addCORSHeaders(w http.ResponseWriter, host string) http.ResponseWriter {
+	w.Header().Add("Access-Control-Allow-Origin", host)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type")
+
+	return w
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	// If OPTION, return just CORS headers
+	if r.Method == http.MethodOptions {
+		w = addCORSHeaders(w, r.Host)
+		w.Header().Add("Access-Control-Max-Age", "1728000") // 20 days
+		w.Header().Add("Content-Type", "text/plain charset=UTF-8")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	// Only allow POSTs
+	if r.Method != http.MethodPost {
+		http.Error(w, "Not a valid method.", http.StatusBadRequest)
+		return
+	}
+
+	w = addCORSHeaders(w, r.Host)
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Request Body Read Error: %+v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	event, err := validateJson(data)
+	if err != nil {
+		http.Error(w, "Not valid data.", http.StatusBadRequest)
+		return
+	}
+
+	json_str, err := json.Marshal(event)
+	if err != nil {
+		log.Printf("JSON Marshal Error: %+v", err)
+		http.Error(w, "Could not turn struct to string.", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", json_str) // send data to client side
+}
+
+type EventMsg struct {
+	Timestamp time.Time `json:"timestamp"`
+	Uuid      string    `json:"uuid"`
+}
+
+func validateJson(msg []byte) (EventMsg, error) {
+	var m EventMsg
+	err := json.Unmarshal(msg, &m)
+	if err != nil {
+		log.Printf("Unmarshaling Error: \"%+v\" for message: %s", err, msg)
+	} else {
+		log.Printf("Parsed Data: %+v", m)
+	}
+
+	return m, err
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/api", apiHandler)
+	http.HandleFunc("/health", healthHandler)
+
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
